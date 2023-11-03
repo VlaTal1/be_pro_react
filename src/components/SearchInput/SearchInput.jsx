960dfaadd550e6e5738040334aa4c7cafd7b9efc
VladyslavTalakh <vladyslav.talakh@sytoss.com>
2023-11-03 15:12:59 +0200

    Added SearchIcon component

---
+++ src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './SearchInput.css';
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Search" className="search-input"></input>
+    </div>
+  );
+};
