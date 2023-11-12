fb709e7d90ea2d81709dbe39d6c2c149fe2970a2
VladyslavTalakh <vladiktalah@gmail.com>
2023-11-12 17:56:42 +0200

    Added Main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import {Header} from "./components/Header/Header";
+import {AppRoutes} from './components/Router/AppRoutes'
 
 function App() {
     return (
         <div className="App">
+            <Header/>
+            <AppRoutes/>
         </div>
     );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Home.css'
+import {MainMenu} from "../../components/MainMenu/MainMenu";
+import {Workspaces} from "../../components/Workspaces/Workspaces";
+
+export const Home = () => {
+    const workspaces = [
+        {
+            title: "Study",
+            boards: [
+                {
+                    title: "JS",
+                },
+                {
+                    title: "Logic",
+                },
+                {
+                    title: "OOP",
+                },
+                {
+                    title: "NoSQL",
+                },
+                {
+                    title: "Algorithms",
+                },
+            ]
+        },
+        {
+            title: "Work",
+            boards: [
+                {
+                    title: "JS",
+                },
+                {
+                    title: "DB",
+                },
+                {
+                    title: "Algorithms",
+                },
+            ]
+        }
+    ]
+
+    return (
+        <div className="home">
+            <MainMenu workspaces={workspaces}/>
+            <Workspaces workspaces={workspaces} title="YOUR WORKSPACES"/>
+        </div>
+    );
+};
