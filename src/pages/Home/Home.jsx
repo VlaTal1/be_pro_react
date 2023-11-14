44921912df17ffd87f7db0adca6f5cacb796e674
VladyslavTalakh <vladiktalah@gmail.com>
2023-11-14 09:35:14 +0200

    Added Templates to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Home.css'
 import {MainMenu} from "./components/MainMenu/MainMenu";
 import {Workspaces} from "./components/Workspaces/Workspaces";
-import {BoardCard} from "./components/BoardCard/BoardCard";
-import pd from '../../images/templates/project_design.png'
+import kanban from '../../images/templates/kanban.png'
+import agile from '../../images/templates/agile.png'
+import scrum from '../../images/templates/scrum.png'
+import game_development from '../../images/templates/game_development.png'
+import project_design from '../../images/templates/project_design.png'
+import system_design from '../../images/templates/system_design.png'
+import game_design from '../../images/templates/game_design.png'
+import design_sprint from '../../images/templates/design_sprint.png'
+import {Templates} from "./components/Templates/Templates";
 
 export const Home = () => {
+    const [content, setContent] = useState("boards")
+
+    const handleContent = (contentType) => {
+        setContent(contentType)
+    }
+
     const workspaces = [
         {
             title: "Study",
@@ -43,11 +56,62 @@ export const Home = () => {
         }
     ]
 
+    const templates = [
+        {
+            name: "Engineering templates",
+            boards: [
+                {
+                    title: "Kanban",
+                    img: kanban,
+                },
+                {
+                    title: "Agile",
+                    img: agile,
+                },
+                {
+                    title: "Scrum",
+                    img: scrum
+                },
+                {
+                    title: "Game development",
+                    img: game_development,
+                },
+            ]
+        },
+        {
+            name: "Design templates",
+            boards: [
+                {
+                    title: "Project Design",
+                    img: project_design,
+                },
+                {
+                    title: "System Design",
+                    img: system_design,
+                },
+                {
+                    title: "Game Design",
+                    img: game_design
+                },
+                {
+                    title: "Design Sprint",
+                    img: design_sprint,
+                },
+            ]
+        },
+    ]
+
     return (
         <main className="home">
-            {/*<MainMenu workspaces={workspaces}/>*/}
-            {/*<Workspaces workspaces={workspaces} title="YOUR WORKSPACES"/>*/}
-            <BoardCard title={"Project Design"} img={pd}/>
+            <MainMenu workspaces={workspaces} itemOnClick={handleContent}/>
+            <div className="home-content">
+                {content === "boards" && (
+                    <Workspaces workspaces={workspaces} title="YOUR WORKSPACES"/>
+                )}
+                {content === "templates" && (
+                    <Templates title="TEMPLATES" templates={templates}/>
+                )}
+            </div>
         </main>
     );
 };
--- src/pages/Home/components/MainMenu/MainMenu.jsx
+++ src/pages/Home/components/MainMenu/MainMenu.jsx
@@ -6,14 +6,19 @@ import {TemplatesIcon} from "../../../../components/Icons/TemplatesIcon";
 import {WorkspaceTitle} from "../../../../components/WorkspaceTitle/WorkspaceTitle";
 import {Link} from "react-router-dom";
 import {LinesIcon} from "../../../../components/Icons/LinesIcon";
-import {SearchInput} from "../../../../components/Header/SearchInput/SearchInput";
 
-export const MainMenu = ({workspaces}) => {
+export const MainMenu = ({workspaces, itemOnClick}) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [activeItem, setActiveItem] = useState("boards")
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
+
+    const handleActiveItem = (itemName) => {
+        setActiveItem(itemName)
+        setIsMenuOpen(!isMenuOpen)
+    }
 
     return (
         <aside className={`main-menu ${isMenuOpen ? 'open' : 'close'}`}>
@@ -22,9 +27,13 @@ export const MainMenu = ({workspaces}) => {
             </div>
             <nav>
                 <ul className="main-menu-header">
-                    <li><WorkspaceMenuItem icon={<BoardIcon/>} title="Boards" isActive={true} onClick={() => {
+                    <li><WorkspaceMenuItem icon={<BoardIcon/>} title="Boards" isActive={activeItem === "boards"} onClick={() => {
+                        itemOnClick("boards")
+                        handleActiveItem("boards")
                     }}/></li>
-                    <li><WorkspaceMenuItem icon={<TemplatesIcon/>} title="Templates" isActive={false} onClick={() => {
+                    <li><WorkspaceMenuItem icon={<TemplatesIcon/>} title="Templates" isActive={activeItem === "templates"} onClick={() => {
+                        itemOnClick("templates")
+                        handleActiveItem("templates")
                     }}/></li>
                 </ul>
                 <div className="main-menu-bottom">
