8e1021b4b8bdea6393eb8db94843968f8672460b
VladyslavTalakh <vladiktalah@gmail.com>
2023-11-14 08:08:08 +0200

    Added burger menu

---
+++ src/components/Icons/LinesIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {IconContainer} from "./IconContainer/IconContainer";
+
+export const LinesIcon = () =>
+    <IconContainer>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none">
+            <path d="M3 3H21V5H3V3ZM3 7H21V9H3V7ZM3 11H21V13H3V11ZM3 15H21V17H3V15ZM3 19H21V21H3V19Z"
+                  fill="var(--accent-color)"/>
+        </svg>
+    </IconContainer>
--- src/pages/Home/components/MainMenu/MainMenu.jsx
+++ src/pages/Home/components/MainMenu/MainMenu.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MainMenu.css'
 import {WorkspaceMenuItem} from "../../../../components/WorkspaceMenuItem/WorkspaceMenuItem";
 import {BoardIcon} from "../../../../components/Icons/BoardIcon";
 import {TemplatesIcon} from "../../../../components/Icons/TemplatesIcon";
 import {WorkspaceTitle} from "../../../../components/WorkspaceTitle/WorkspaceTitle";
 import {Link} from "react-router-dom";
+import {LinesIcon} from "../../../../components/Icons/LinesIcon";
+import {SearchInput} from "../../../../components/Header/SearchInput/SearchInput";
 
 export const MainMenu = ({workspaces}) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <aside className={"main-menu"}>
+        <aside className={`main-menu ${isMenuOpen ? 'open' : 'close'}`}>
+            <div className="burger-menu" onClick={toggleMenu}>
+                <LinesIcon/>
+            </div>
             <nav>
                 <ul className="main-menu-header">
                     <li><WorkspaceMenuItem icon={<BoardIcon/>} title="Boards" isActive={true} onClick={() => {
